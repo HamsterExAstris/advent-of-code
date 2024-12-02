9a85f13f56e9fd280a5164a8bfb8ec8f2ced4079
Andrew Timson <6879630+HamsterExAstris@users.noreply.github.com>
2024-12-01 22:13:07 -0500

    2024-12-01 part 1

---
+++ src/2024/01/Program.cs
@@ -0,0 +1,21 @@
+﻿using var file = new StreamReader("input.txt");
+var list1 = new List<int>();
+var list2 = new List<int>();
+var nextLine = await file.ReadLineAsync();
+while (nextLine != null)
+{
+    var pieces = nextLine.Split(' ',
+        StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+    list1.Add(int.Parse(pieces[0]));
+    list2.Add(int.Parse(pieces[1]));
+    nextLine = await file.ReadLineAsync();
+}
+list1.Sort();
+list2.Sort();
+
+var distance = 0;
+for (int x = 0; x < list1.Count; x++)
+{
+    distance += Math.Abs(list1[x] - list2[x]);
+}
+Console.WriteLine(distance);
